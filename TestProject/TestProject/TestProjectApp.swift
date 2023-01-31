97af08dccdcc6032c4b60139615c28f61336dd80
Ashu <Ashu@users.noreply.github.com>
2023-01-31 15:40:06 +0530

    Initial commit

---
+++ TestProject/TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Ashu on 31/01/23.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
